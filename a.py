2c85f758e29c0ed117a90d9cab616c70f17ad6b7
k-jank <157026427+k-jank@users.noreply.github.com>
2024-08-12 20:10:06 +0700

    Update a.py

--- a.py
+++ a.py
@@ -13,8 +13,7 @@ def text_to_speech(text):
 st.title("Text-to-Speech dengan gTTS")
 
 # Input teks dari pengguna
-text_input = st.text_area("Masukkan teks:", "Tidaklah tepat untuk mengatakan bahwa semua orang di negara-negara industri yang belum berkembang yang tidak dipengaruhi oleh agama Kristen atau Islam ortodoks percaya pada reinkarnasi. 
-Beberapa orang tetap bertahan dengan ajaran-ajaran ini dan tetap mempertahankan agama-agama tradisional yang tidak menyertakan kepercayaan ini. Namun, beberapa pengecualian ini, sedikit mengurangi generalisasi bahwa hampir semua orang di luar jangkauan Kristen ortodoks, Yahudi, Islam, dan Sains - yang terakhir adalah agama sekuler bagi banyak orang - percaya pada reinkarnasi. Saya akan mempertimbangkan selanjutnya bagaimana hal ini bisa terjadi.")
+text_input = st.text_area("Masukkan teks:", "Tidaklah tepat untuk mengatakan bahwa semua orang di negara-negara industri yang belum berkembang yang tidak dipengaruhi oleh agama Kristen atau Islam ortodoks percaya pada reinkarnasi. Beberapa orang tetap bertahan dengan ajaran-ajaran ini dan tetap mempertahankan agama-agama tradisional yang tidak menyertakan kepercayaan ini. Namun, beberapa pengecualian ini, sedikit mengurangi generalisasi bahwa hampir semua orang di luar jangkauan Kristen ortodoks, Yahudi, Islam, dan Sains - yang terakhir adalah agama sekuler bagi banyak orang - percaya pada reinkarnasi. Saya akan mempertimbangkan selanjutnya bagaimana hal ini bisa terjadi.")
 
 if st.button("Convert to Speech"):
     audio_data = text_to_speech(text_input)
