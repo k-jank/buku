d02d3740ec94182a2787e016727be3610428abb6
k-jank <157026427+k-jank@users.noreply.github.com>
2024-08-12 17:15:29 +0700

    Update a.py

--- a.py
+++ a.py
@@ -83,16 +83,14 @@ def text_to_speech(text):
     voices = engine.getProperty('voices')
     engine.setProperty('voice', voices[1].id)  # Set the voice to the second option
     engine.setProperty('rate', 170)  # Set speech rate to 170 words per minute
-    
-    # Save the speech to a temporary file and read its content
-    with tempfile.NamedTemporaryFile(delete=False, suffix='.mp3') as temp_file:
-        temp_file.close()
-        engine.save_to_file(text, temp_file.name)
-        engine.runAndWait()
-        with open(temp_file.name, 'rb') as audio_file:
-            audio_data = audio_file.read()
-        return audio_data
 
+    # Save the speech to a temporary file
+    with tempfile.NamedTemporaryFile(delete=False, suffix='.mp3') as temp_file:
+        temp_file_path = temp_file.name
+        engine.save_to_file(text, temp_file_path)
+        engine.runAndWait()
+        return temp_file_path
+        
 # Streamlit app
 st.markdown("""
     <style>
@@ -144,7 +142,8 @@ if selected_title != "Pilih Buku...":
         # Button for converting text to speech
         if st.button("Dengarkan Audio"):
             audio_file_path = text_to_speech(chapter_text)
-            st.audio(audio_data, format='audio/mp3')
+            st.audio(audio_file_path, format='audio/mp3')
+
         
         # Display the selected chapter content in a collapsible section with title
         with st.expander(f"Tampilkan Isi Buku: {selected_chapter}"):
